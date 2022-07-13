6078a71852668c618a334213da2d66493d24c414
alexdietz1988 <alex.dietz@gmail.com>
2022-07-13 09:43:50 -0400

    Get/update/delete meal and get/update user info

--- controllers/meal.js
+++ controllers/meal.js
@@ -11,4 +11,32 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/', async (req, res) => {
+    try {
+        let meals = await db.Meal.find({username: req.query.username})
+        res.json(meals)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.put('/', async (req, res) => {
+    try {
+        console.log(req.query.id)
+        await db.Meal.findByIdAndUpdate(req.query.id, req.body)
+        res.json('successfully updated meal')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.delete('/', async (req, res) => {
+    try {
+        await db.Meal.findByIdAndDelete(req.query.id)
+        res.json('successfully deleted meal')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
 module.exports = router
--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -4,8 +4,26 @@ const router = express.Router()
 
 router.post('/', async (req, res) => {
     try {
-        let newUserInfo = await db.UserInfo.create(req.body)
-        res.json(newUserInfo)
+        await db.UserInfo.create(req.body)
+        res.json('successfully created user info')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        let userInfo = await db.UserInfo.find({username: req.query.username})
+        res.json(userInfo)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.put('/', async (req, res) => {
+    try {
+        await db.UserInfo.findOneAndUpdate(req.params.username, req.body)
+        res.json('successfully updated user info')
     } catch (error) {
         res.status(400).json(error)
     }
