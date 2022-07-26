bba570bd85eb948d017f730564fd0a1b6e7fbcdf
alexdietz1988 <alex.dietz@gmail.com>
2022-07-26 09:27:01 -0400

    Signup creates user info

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
+const db = require('../models')
 
 router.post('/signup', async (req, res) => {
     try {
-        const foundUser = await User.exists({username: req.body.username})
+        const foundUser = await db.User.exists({username: req.body.username})
         if (foundUser) return res.json('user already exists')
 
         const salt = await bcrypt.genSalt(12)
         const hash = await bcrypt.hash(req.body.password, salt)
         req.body.password = hash
 
-        await User.create(req.body)
+        await db.User.create(req.body)
+        await db.UserInfo.create({username: req.body.username, weight: ' ', goal: ' '})
         return res.json('user created')
     } catch (error) {
         console.log(error)
@@ -21,9 +22,18 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        await db.UserInfo.create(req.body)
+        res.json('successfully created user info')
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
 router.post('/login', async (req, res) => {
     try {
-        const foundUser = await User.findOne({ username: req.body.username })
+        const foundUser = await db.User.findOne({ username: req.body.username })
         const match = await bcrypt.compare(req.body.password, foundUser.password)
         if(!match) return res.json('invalid username or password')
         return res.json('successfully logged in')
--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -2,15 +2,6 @@ const express = require('express')
 const db = require('../models')
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-    try {
-        await db.UserInfo.create(req.body)
-        res.json('successfully created user info')
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
-
 router.get('/', async (req, res) => {
     try {
         let userInfo = await db.UserInfo.find({username: req.query.username})
